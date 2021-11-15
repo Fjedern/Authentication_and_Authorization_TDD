846d9d6da250ede62a66c79b48b2b2b9181fb010
David <david@nlsn.nu>
2021-11-15 19:50:32 +0100

    Update LoginHandlerTest.java

--- src/test/java/org/example/LoginHandlerTest.java
+++ src/test/java/org/example/LoginHandlerTest.java
@@ -2,6 +2,7 @@ package org.example;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class LoginHandlerTest {
@@ -13,5 +14,7 @@ public class LoginHandlerTest {
         Login login = loginHandler.startLogin("anna", "losen");
 
         assertNotNull(login);
+        assertEquals("anna", login.getUsername());
+        assertEquals("losen", login.getPassword());
     }
 }
